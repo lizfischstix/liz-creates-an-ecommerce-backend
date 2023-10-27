f4f55cfd5e1c90a2bd592b0ca99ff8cbae1c483d
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-10-27 17:38:36 -0500

    tag routs connected up to get by ID.  TODO finish connecting tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,27 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tags = await Tag.findAll({
+    });
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const getTagID = await Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(getTagID);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
